87c753523add4f8c09740398656990bea50a378e
Patrick Meehan <patrickmeehan.git@fallguyconsulting.com>
2024-04-02 01:47:34 -0700

    updated deploy

--- pull.sh
+++ pull.sh
@@ -3,5 +3,5 @@
 SCRIPT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 pushd ${SCRIPT_HOME}
-    docker pull gamercert/identibot
+    docker pull fallguydev/identibot-js
 popd
